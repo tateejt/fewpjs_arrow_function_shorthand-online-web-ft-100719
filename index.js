ef35608cb3bd043a12be17a8c6fbe1bc756b4430
Emery Tate <tateejt@gmail.com>
2020-02-12 06:08:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function divide() {
-  let divides = 2000 / 100;
-  return divides;
-}
-
-function square() {
-  let add = (params1, params2) => {
-    return params1 * params2;
-  }
-  add(2,2);
-}
+let divide = () => 2000/100
+let square = x => x * x;
+let add = (a, b) => a + b;
